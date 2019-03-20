5aee34cd9f57972c76558078541eff232553fac6
oscvic <vicenteyan@gmail.com>
2019-03-20 16:41:22 -0600

    cambio

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+        <Text>Hello, Oscar!!!</Text>
       </View>
     );
   }
